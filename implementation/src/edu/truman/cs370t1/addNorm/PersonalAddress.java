7381d35ecbc19f224e9ce1d091e9e0a848c25006
Yuhao Zhang <yhzhang815@gmail.com>
2016-04-12 22:27:08 -0500

    Began user story Zip

--- implementation/src/edu/truman/cs370t1/addNorm/PersonalAddress.java
+++ implementation/src/edu/truman/cs370t1/addNorm/PersonalAddress.java
@@ -1,11 +1,36 @@
 package edu.truman.cs370t1.addNorm;
 
 public class PersonalAddress extends Address {
+	
+
+
+	public PersonalAddress(String zip4, String zip5) {
+		this.zip4 = zip4;
+		this.zip5 = zip5;		
+	}
 
 	@Override
 	public boolean normalize() {
-		// TODO Auto-generated method stub
-		return false;
+		if(zip4.length() != 4){
+			return false;
+		}
+		for(char character : zip4.toCharArray()){
+			if(!Character.isDigit(character)){
+				return false;
+			}
+		}
+		if(zip5.length() != 5){
+			return false;
+		}
+		for(char character : zip5.toCharArray()){
+			if(!Character.isDigit(character)){
+				return false;
+			}
+		}
+			
+		
+		
+		return true;
 	}
 
 }
--- implementation/test/edu/truman/cs370t1/addNormTest/PersonalAddressTest.java
+++ implementation/test/edu/truman/cs370t1/addNormTest/PersonalAddressTest.java
@@ -4,17 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import edu.truman.cs370t1.addNorm.Address;
 import edu.truman.cs370t1.addNorm.PersonalAddress;
 
 public class PersonalAddressTest {
+	
+	Address address;
+	Address address2;
 
 	@Before
 	public void setUp() throws Exception {
+		address = new PersonalAddress("1234","12345");
+		address2 = new PersonalAddress("ABCD","ABCDE");
 	}
 
 	@Test
 	public void testNormalize() {
-		fail("Not yet implemented");
+		assertTrue(address.normalize());
+		assertFalse(address2.normalize());
 	}
 
 }
