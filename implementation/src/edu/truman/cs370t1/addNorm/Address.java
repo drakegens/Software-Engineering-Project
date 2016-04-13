a07e84927d81c7f55c618958b3633173f8dc4e1b
Drake Gens <dmg3881@truman.edu>
2016-04-12 22:59:46 -0500

    Began parsing user story development, DG PM

--- implementation/src/edu/truman/cs370t1/addNorm/Address.java
+++ implementation/src/edu/truman/cs370t1/addNorm/Address.java
@@ -8,10 +8,73 @@ protected String zip5;
 protected String zip4;
 protected String line1;
 protected String flag;
+protected String city;
 
 
 	
+	public String getCity() {
+	return city;
+}
+
+
+
+public void setCity(String city) {
+	this.city = city;
+}
+
+
+
 	public abstract boolean normalize();
+
+
+
+	public String getStateCode() {
+		return stateCode;
+	}
+
+
+
+	public void setStateCode(String stateCode) {
+		this.stateCode = stateCode;
+	}
+
+
+
+	public String getZip5() {
+		return zip5;
+	}
+
+
+
+	public void setZip5(String zip5) {
+		this.zip5 = zip5;
+	}
+
+
+
+	public String getZip4() {
+		return zip4;
+	}
+
+
+
+	public void setZip4(String zip4) {
+		this.zip4 = zip4;
+	}
+
+
+
+	public String getLine1() {
+		return line1;
+	}
+
+
+
+	public void setLine1(String line1) {
+		this.line1 = line1;
+	}
+	
+	
 		
 		
 		
--- implementation/test/edu/truman/cs370t1/addNormTest/CSVReaderTest.java
+++ implementation/test/edu/truman/cs370t1/addNormTest/CSVReaderTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import edu.truman.cs370t1.addNorm.CSVReader;
+import edu.truman.cs370t1.addNorm.Address;
+import edu.truman.cs370t1.addNorm.PersonalAddress;
 
 public class CSVReaderTest {
 
@@ -18,6 +21,21 @@ public class CSVReaderTest {
 	
 	@Test 
 	public void testParse() {
+		CSVReader cr = new CSVReader();
+		//assertEquals(new PersonalAddress(), cr.parse());
+		assertNotNull(cr.parse());
+		Address add = cr.parse();
+		//cr.parse();
+		assertEquals("123 Normal St", add.getLine1());
+		
+		assertEquals("Kirksville", add.getCity());
+		
+		assertEquals("MO", add.getStateCode());
+		
+		assertEquals("63501", add.getZip5());
+		
+		assertEquals("1234", add.getZip4());
+		
 		
 	}
 
