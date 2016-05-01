85b7edb624920724da0dd10f35896ac9ab052ff2
Yuhao Zhang <yhzhang815@gmail.com>
2016-05-01 14:37:10 -0500

    Continued working on GUI

--- implementation/src/AddNormSingle.java
+++ implementation/src/AddNormSingle.java
@@ -8,10 +8,38 @@ public class AddNormSingle extends JFrame{
 	private JTextField addressInput;
 	private JButton normalizeButton;
 	
+	public AddNormSingle(){
+		Container cp = getContentPane();
+		cp.setLayout(new FlowLayout());
+		cp.add(new JLabel("Address: "));
+		addressInput = new JTextField("Please enter address here");
+		addressInput.setEditable(true);
+		cp.add(addressInput);
+		normalizeButton = new JButton("Normalize");
+		cp.add(normalizeButton);
+		normalizeButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Address Normalizer");
+		setSize(300, 100);
+		setVisible(true);
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new AddNormSingle();
+				
+			}
+		});
+		
 	}
 
 }
