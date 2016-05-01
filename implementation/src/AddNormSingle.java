4680c974203ba8d809600826aa74963a19c3aebb
Yuhao Zhang <yhzhang815@gmail.com>
2016-05-01 14:27:47 -0500

    Started working on GUI part

---
+++ implementation/src/AddNormSingle.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;;
+
+
+public class AddNormSingle extends JFrame{
+	
+	private JTextField addressInput;
+	private JButton normalizeButton;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
